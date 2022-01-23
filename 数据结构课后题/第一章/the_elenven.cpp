a6355d0a4f69c2dc655823f7e5861e9f0ef3aab0
ChenWeihao <3045696670@qq.com>
2022-01-23 19:15:27 +0800

    改善了，但是引用传数组不行

--- 数据结构课后题/第一章/the_elenven.cpp
+++ 数据结构课后题/第一章/the_elenven.cpp
@@ -1,9 +1,10 @@
-// ps: 这里只做了指针传参的形式，*/
+// ps: 这里只做了指针传参的形式，
+// 引用传参还没有做出来
 #include <iostream>
 using namespace std;
 
 template <class T>
-int count(T *num, T _value, int length) // 通过指针传参
+int count1(T *num, T _value, int length) // 通过指针传参
 {
     int count_num = 0;
     if (length < 1)
@@ -19,12 +20,12 @@ int count(T *num, T _value, int length) // 通过指针传参
     return count_num;
 }
 
-template <class T, int N = 20>
-int count(T (&num)[N], T _value) // 通过引用传参
+template <class T, int &N>
+int count2(T (&num)[N], T _value) // 通过引用传参  引用是数组格式是： type (&数组名) [数组长度]
 {
     int count_num = 0;
-    int length = sizeof(num) / sizeof(num[0]);
-    for (int i = 0; i < length; i++)
+    /* int length = sizeof(num) / sizeof(num[0]); */
+    for (int i = 0; i < N; i++)
     {
         if (num[i] == _value)
             count_num++;
@@ -35,17 +36,19 @@ int count(T (&num)[N], T _value) // 通过引用传参
 
 int main(void)
 {
-    int len;
+    int len = 0;
+    cout << "输入数组的长度：";
     cin >> len;
     int *n = new int[len];
 
     int number;
+    cout << "请输入数字：" << endl;
     cin >> number;
 
     for (int i = 0; i < len; i++)
     {
         n[i] = number;
-        if (i == len - 1)
+        if (i == len - 1) // i==数组长度-1 时，不再输入数字，跳出循环
         {
             break;
         }
@@ -53,13 +56,14 @@ int main(void)
     }
 
     int _value;
+    cout << "输入要查找的数字：";
     cin >> _value;
 
     try
     {
-        cout << count(n, _value, len) << endl;
+        cout << _value << "在数组中有" << count1(n, _value, len) << "个！" << endl;
     }
-    catch (const char *error)   // 捕获参数应与抛出参数类型一致，这里为 const char*
+    catch (const char *error) // 捕获参数应与抛出参数类型一致，这里为 const char*
     {
         cout << "错误！" << endl;
         cout << error << endl;
