2c94dad55604b3536778029b303ac733f1cff528
陈伟浩 <cwh30456966670cwh@gmail.com>
2022-01-25 19:17:11 +0800

    make 1d Array

---
+++ 数据结构课后题/第一章/Make1dArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+template <class T> // 模板头
+// 创建一维数组
+void make1dArray(T *&x, int numberOfRows)
+{ //  x为指向new分配内存的指针的引用 ， numberOfRows 为数组的长度
+    x = new T[numberOfRows];
+}
+
+// 释放数组的空间
+template <typename T>
+void delete1dArray(T *&x)
+{
+    delete[] x;
+    x = nullptr; // 将 x 指向空，防止用户访问已被释放的空间
+}
+
+int main(void)
+{
+    int *x;           // 定义一个指针
+    int numberOfRows; // 数组长度
+    cout << "一维数组的长度为：";
+    cin >> numberOfRows;
+
+    try
+    {
+        make1dArray(x, numberOfRows); // 把可能出现的异常放在try块中
+    }
+
+    catch (bad_alloc e)
+    { // 操作符new 分配内存失败， 返回一个类型为 bad_alloc 的异常，
+        cerr << "Out of Merroy" << endl;
+        exit(1); // 非正常运行导致程序退出
+    }
+
+    delete1dArray(x); // 调用释放数组内存的函数
+
+    return 0;
+}
